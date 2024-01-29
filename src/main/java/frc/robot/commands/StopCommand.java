ef7382b40f0cff8f284ce31571397867b01984e9
Robovikes <robovikes@geneva304.org>
2024-01-29 16:16:32 -0600

    stop command

---
+++ src/main/java/frc/robot/commands/StopCommand.java
@@ -0,0 +1,17 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.DriveSubsystem;
+
+public class StopCommand  {
+    DriveSubsystem driveSubsystem;
+
+    public StopCommand(DriveSubsystem drive) {
+        driveSubsystem = drive;
+        
+    }
+
+    @Override
+    public void execute(){
+        driveSubsystem.stop();
+    }
+}
