d43fdcc09e604f85460776ce3feab96abb1d7d8b
Collyn Diamond <thediamonds0813@gmail.com>
2024-02-03 16:33:53 -0600

    nick doesnt know how to code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -26,9 +26,12 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
@@ -40,53 +43,60 @@ public class RobotContainer {
   /* ~~~~Commands~~~~ */
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
-  private final CommandXboxController controlController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private final CommandXboxController driverController = new CommandXboxController(
+      OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController controlController = new CommandXboxController(
+      OperatorConstants.kDriverControllerPort);
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
 
   SendableChooser<String> autoChooser = new SendableChooser<>();
 
-
-
   public RobotContainer() {
     autoChooser.setDefaultOption("path1", "path 1");
-
 
     // Configure the trigger bindings
     configureBindings();
   }
 
-  public void checkLimitSwitch(){
+  public void checkLimitSwitch() {
 
   }
 
-  public void encoderTest(){
+  public void encoderTest() {
 
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    /*new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());*/
+    /*
+     * new Trigger(m_exampleSubsystem::exampleCondition)
+     * .onTrue(new ExampleCommand(m_exampleSubsystem));
+     * 
+     * // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+     * pressed,
+     * // cancelling on release.
+     * m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+     */
   }
 
-  public Command getTeleopCommand(){
-    return new ParallelCommandGroup(
-      new TeleopCommand(driveSubsystem, driverController)
-    );
+  public Command getTeleopCommand() {
+    return new TeleopCommand(driveSubsystem, driverController);
   }
 
   /**
@@ -96,6 +106,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return new ParallelCommandGroup(/*TODO */);
+    return new ParallelCommandGroup(/* TODO */);
   }
 }
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,35 +1,22 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.ADIS16448_IMU;
-import frc.robot.commands.StopCommand;
 
-public ArmSubsystem extends SubsystemBase{
+public class ArmSubsystem extends SubsystemBase {
     TalonFX leftArmMotor;
     TalonFX rightArmMotor;
     TalonFX extensionMotor;
 
-    public ArmSubsystem(){
+    public ArmSubsystem() {
         leftArmMotor = new TalonFX(8);
         rightArmMotor = new TalonFX(9);
-        extensionMotor new TalonFX(10);
+        extensionMotor = new TalonFX(10);
 
-
-        leftArmMotor.setNeutralMode(neutralMode.Brake);
-        rightArmMotor.setNuetralMode(neutralMode.Brake);
     }
 
-    //sets speed for the extension motor
-    public void setExtensionMotor(double speed){
+    // sets speed for the extension motor
+    public void setExtensionMotor(double speed) {
         extensionMotor.set(speed);
     }
 }
