f3e1ca4bf418b8fa10574e42d1a4db1de2a725df
darkschild <156546369+darkschild@users.noreply.github.com>
2024-02-27 10:15:11 -0600

    arm subsysem and comand plus cotroles partialy set up for arm

--- src/main/java/frc/robot/subsystems/armsubsytem.java
+++ src/main/java/frc/robot/subsystems/armsubsytem.java
@@ -10,37 +10,55 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class armsubsytem extends SubsystemBase {
-  TalonFX falcon1;
-  TalonFX falcon2;
-  TalonFX falcon3;
-  TalonFX falcon4;
-  DigitalInput limitswitch;
+  TalonFX mainpivotmotor;
+  TalonFX intakepivotmotor;
+  TalonFX extension1motor;
+  TalonFX extension2motor;
+  DigitalInput mainpivot;
+  DigitalInput intakepivot;
+  DigitalInput extensionoutlimit;
+  DigitalInput extensioninlimit;
   /** Creates a new armsubsytem. */
   public armsubsytem() {
-    falcon1 = new TalonFX(61);
-    falcon2 = new TalonFX(62);
-    falcon3 = new TalonFX(63);
-    falcon4 = new TalonFX(64);
-    limitswitch = new DigitalInput(1);
+    mainpivotmotor = new TalonFX(61);
+    intakepivotmotor = new TalonFX(62);
+    extension1motor = new TalonFX(63);
+    extension2motor = new TalonFX(64);
+    mainpivot = new DigitalInput(1);
+    intakepivot = new DigitalInput(2);
+    extensionoutlimit = new DigitalInput(3);
+    extensioninlimit = new DigitalInput(4);
   }
 public void setfalcon1(double speed){
-  falcon1.set(speed);
+  mainpivotmotor.set(speed);
 }
 
 public void setfalcon2(double speed){
-  falcon2.set(speed);
+  intakepivotmotor.set(speed);
 }
 
 public void setfalcon3(double speed){
-  falcon3.set(speed);
+  extension1motor.set(speed);
 }
 
 public void setfalcon4(double speed){
-  falcon4.set(speed);
+  extension2motor.set(speed);
 }
 
-public boolean getlimit() {
-  return limitswitch.get();
+public boolean getlimit1() {
+  return mainpivot.get();
+}
+
+public boolean getlimit2() {
+  return intakepivot.get();
+}
+
+public boolean getlimit3() {
+  return extensionoutlimit.get();
+}
+
+public boolean getlimit4() {
+  return extensioninlimit.get();
 }
 
   @Override
